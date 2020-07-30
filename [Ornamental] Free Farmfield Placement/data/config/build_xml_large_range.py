18b9eeb22af3932c9d69e30dcdb58f8c01771904
4kkz0ck7 <53898321+akkzockt@users.noreply.github.com>
2020-07-30 21:50:39 +0200

    [Ornamental] Free Farmfield Placement

---
+++ [Ornamental] Free Farmfield Placement/data/config/build_xml_large_range.py
@@ -0,0 +1,46 @@
+import json, sys, os, math
+
+# Check 190865 for all GUIDs when updating the json
+
+def radius_function(data):
+	return int(data["module_limit"])
+
+def generate_farm_module_owner(data):
+	xml = """	<ModOp GUID = '{GUID}' Path="Values/ModuleOwner" Type="replace">
+		<ModuleOwner>
+			<ModuleLimits>
+				<Main>
+					<Limit>{module_limit}</Limit>
+				</Main>
+			</ModuleLimits>
+			<ConstructionOptions>
+				<Item>
+					<ModuleGUID>{module_GUID}</ModuleGUID>
+				</Item>
+			</ConstructionOptions>
+			<ModuleBuildRadius>{radius}</ModuleBuildRadius>
+			<HardFarmsConfig>{hard_farms}</HardFarmsConfig>
+		</ModuleOwner>				 
+	</ModOp>""".format(radius = radius_function(data), **data)
+	return xml
+
+
+try:
+	if __name__ == "__main__":
+		with open("farm_data.json") as farm_data_file:
+			farm_data = json.load(farm_data_file)	
+		
+		mod_Ops = []
+		for entry in farm_data["FarmData"]:
+			mod_Ops.append(generate_farm_module_owner(entry))
+			
+		# write asset file
+		with open("export/main/asset/assets.xml", "w") as file:
+			file.write("<ModOps>\n")		
+			file.write("\n".join(mod_Ops))
+			file.write("\n</ModOps>") 
+			
+except Exception as err:
+	print(err)
+	print("Unexpected error:", sys.exc_info()[2].tb_lineno)
+	input()
